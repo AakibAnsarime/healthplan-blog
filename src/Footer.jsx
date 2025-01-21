75ed9d16611d80eb500157cbc0590e21a57c2358
AakibAnsari <aakiba6619@gmail.com>
2025-01-21 21:56:36 +0300

    footer update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-green-400 py-8">
       <div className="container mx-auto px-8">
         <div className="flex flex-col md:flex-row items-center justify-between">
           {/* Footer Text */}
-          <p className="text-center md:text-left text-sm mb-4 md:mb-0">
+          <p className="text-center md:text-left text-l mb-4 md:mb-0">
             &copy; {new Date().getFullYear()} My Health Plan. All rights reserved.
           </p>
           
@@ -15,13 +15,13 @@ const Footer = () => {
           <div className="text-center md:text-right">
             <Link 
               to="/terms-and-conditions" 
-              className="text-blue-400 hover:text-blue-500 transition duration-200 ease-in-out text-sm"
+              className="hover:text-white transition duration-200 ease-in-out text-l"
             >
               Terms and Conditions
             </Link>
             <Link 
               to="/privacy-policy" 
-              className="text-blue-400 hover:text-blue-500 transition duration-200 ease-in-out text-sm ml-4"
+              className="hover:text-white transition duration-200 ease-in-out text-l ml-4"
             >
               Privacy Policy
             </Link>
