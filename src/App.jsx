fba61f057f6111ab29f14bfcb3b8f57c7882e685
AakibAnsari <aakiba6619@gmail.com>
2025-01-26 11:10:43 +0300

    meta update

--- src/App.jsx
+++ src/App.jsx
@@ -23,26 +23,27 @@ const App = () => {
         <Helmet>
           <title>Healthblog - Your Source for Fitness and Wellness</title>
           <meta name="description" content="Healthblog provides the latest tips and advice on fitness, wellness, and healthy living. Join our community and start your journey to a healthier life today!" />
-          <link rel="canonical" href="https://www.yourwebsite.com/" />
+          <link rel="canonical" href="https://www.myhealthplanblog.com" />
           <meta property="og:title" content="Healthblog - Your Source for Fitness and Wellness" />
           <meta property="og:description" content="Healthblog provides the latest tips and advice on fitness, wellness, and healthy living. Join our community and start your journey to a healthier life today!" />
-          <meta property="og:url" content="https://www.yourwebsite.com/" />
+          <meta property="og:url" content="https://www.myhealthplanblog.com" />
           <meta property="og:type" content="website" />
-          <meta property="og:image" content="https://www.yourwebsite.com/path-to-your-image.jpg" />
+          <meta property="og:image" content="https://www.myhealthplanblog.com/logo.png" />
+          <meta name="bb62b1f7f41519a46b6508579145844478784bc6" content="bb62b1f7f41519a46b6508579145844478784bc6" />
           <script type="application/ld+json">
             {`
               {
                 "@context": "https://schema.org",
                 "@type": "WebSite",
-                "url": "https://www.yourwebsite.com/",
-                "name": "Healthblog",
-                "description": "Healthblog provides the latest tips and advice on fitness, wellness, and healthy living. Join our community and start your journey to a healthier life today!",
+                "url": "https://www.myhealthplanblog.com",
+                "name": "Healthplanblog",
+                "description": "Healthplanblog provides the latest tips and advice on fitness, wellness, and healthy living. Join our community and start your journey to a healthier life today!",
                 "publisher": {
                   "@type": "Organization",
-                  "name": "Healthblog",
+                  "name": "Healthplanblog",
                   "logo": {
                     "@type": "ImageObject",
-                    "url": "https://www.yourwebsite.com/path-to-your-logo.jpg"
+                    "url": "https://www.myhealthplanblog.com/logo.png"
                   }
                 }
               }
